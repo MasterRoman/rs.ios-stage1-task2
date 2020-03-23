03dfacce66c186aa682f41bc44a63c9b900c6576
MasterRoman <53902992+MasterRoman@users.noreply.github.com>
2020-03-23 12:10:20 +0300

    feat:solve problem

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,22 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    NSMutableArray *a = [[NSMutableArray alloc]init];
+    [a addObject:@"0"];
+    [a addObject:@"1"];
+    int i = 1;
+    do {
+        i++;
+        [a addObject:[NSNumber numberWithInt:[[a objectAtIndex:(i-2)] intValue] + [[a objectAtIndex:(i-1)] intValue]]];
+    } while (([[a objectAtIndex:i] intValue]*[[a objectAtIndex:i-1] intValue])<[number intValue]);
+    if (([[a objectAtIndex:i] intValue]*[[a objectAtIndex:i-1] intValue])==[number intValue]){
+        NSArray *outp = @[[a objectAtIndex:i-1],[a objectAtIndex:i],@1];
+        return outp;
+        
+    }
+    else {
+        NSArray *outp = @[[a objectAtIndex:i-1],[a objectAtIndex:i],@0];
+        return outp;
+    }
 }
 @end
