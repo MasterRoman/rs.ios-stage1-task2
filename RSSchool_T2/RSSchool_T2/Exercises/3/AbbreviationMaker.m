95dfb93f62b904a64ec579f5c2e20930c5ef355d
MasterRoman <53902992+MasterRoman@users.noreply.github.com>
2020-03-23 12:09:28 +0300

    feat:solve problem

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,28 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    int curPos = 0;
+    int prevPos = 0;
+    int i=0;
+    int j=0;
+    NSMutableString *aCoptyString = [a copy];
+    NSString *aUppcaseString = [aCoptyString uppercaseString];
+        while ((i<a.length) && (j<b.length)) {
+            if ([b characterAtIndex:j] == [aUppcaseString characterAtIndex:i] ){
+                curPos = i;
+                j++;
+                i=-1;
+                if (curPos<prevPos){
+                    return @"NO";
+                } else
+                {prevPos = curPos;}
+            }
+            
+            i++;
+        }
+        if (j<b.length-1){
+            return @"NO";
+        }
+    return @"YES";
 }
 @end
